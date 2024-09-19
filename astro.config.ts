80fa98ee00d81cfcefc9d77efca948a269b32dcb
adit-bala <aditbala@berkeley.edu>
2024-09-18 23:32:07 -0700

    fix build errors

--- astro.config.ts
+++ astro.config.ts
@@ -52,8 +52,6 @@ export default defineConfig({
 	},
 	// https://docs.astro.build/en/guides/prefetch/
 	prefetch: true,
-	// ! Please remember to replace the following site property with your own domain
-	site: "https://astro-cactus.chriswilliams.dev/",
 	vite: {
 		optimizeDeps: {
 			exclude: ["@resvg/resvg-js"],

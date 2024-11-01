10ce7a5bbcd09d61614d40839020667b05845f92
adit-bala <aditbala@berkeley.edu>
2024-11-01 11:25:40 -0700

    add types for linkify

--- src/utils/linkify.ts
+++ src/utils/linkify.ts
@@ -1,36 +1,44 @@
-// Function to process text and replace phrases with links
-export function processText(text: string, links: Record<string, string> | undefined){
-    if (!links) {
-      return [text];
-    }
-  
-    // Escape special regex characters in phrases
-    const escapeRegExp = (string: string) =>
-      string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-  
-    // Create a regex pattern that matches any of the phrases
-    const phrases = Object.keys(links);
-    const regexPattern = phrases
-      .map((phrase) => escapeRegExp(phrase))
-      .join("|");
-    const regex = new RegExp(`(${regexPattern})`, "g");
-  
-    // Split the text based on the regex pattern
-    const parts = text.split(regex);
-  
-    // Map over the parts and replace phrases with links
-    return parts.map((part) => {
-      if (links[part]) {
-        return {
-          type: 'link',
-          href: links[part],
-          text: part
-        };
-      } else {
-        return {
-          type: 'text',
-          content: part
-        };
-      }
-    });
+type LinkPart = {
+  type: 'link';
+  href: string;
+  text: string;
+};
+
+type TextPart = {
+  type: 'text';
+  content: string;
+};
+
+type ProcessedPart = LinkPart | TextPart;
+
+export function processText(text: string, links: Record<string, string> | undefined): ProcessedPart[] {
+  if (!links) {
+    return [{ type: 'text', content: text }];
   }
+
+  const escapeRegExp = (string: string) =>
+    string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+
+  const phrases = Object.keys(links);
+  const regexPattern = phrases
+    .map((phrase) => escapeRegExp(phrase))
+    .join("|");
+  const regex = new RegExp(`(${regexPattern})`, "g");
+
+  const parts = text.split(regex);
+
+  return parts.map((part): ProcessedPart => {
+    if (links[part]) {
+      return {
+        type: 'link',
+        href: links[part],
+        text: part
+      };
+    } else {
+      return {
+        type: 'text',
+        content: part
+      };
+    }
+  });
+}

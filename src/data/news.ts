2a9b99c577eff524a991088e03c93cd385800443
adit-bala <aditbala@berkeley.edu>
2024-11-29 20:31:23 -0800

    chore: reformat homepage timeline

--- src/data/news.ts
+++ src/data/news.ts
@@ -4,13 +4,19 @@ interface NewsItem {
   }
   
   interface NewsFeed {
+    future: NewsItem[];
     currently: NewsItem[];
     previously: NewsItem[];
   }
   
 export const newsFeed: NewsFeed = {
+future: [
+  {text: "codepoint fellow @ Sutter Hill Ventures", links: {"Sutter Hill Ventures": "https://jeffburke.substack.com/p/sutter-hill-ventures-the-silent-builders"}},
+  {text: "cloud infrastructure swe @ Sigma Computing", links: {"Sigma Computing": "https://www.sigmacomputing.com/"}},
+  {text: "swe intern @ Deno", links: {"Deno": "https://deno.com/"}},
+],
+
 currently: [
-    {text: "cloud infrastructure swe @ Sigma Computing", links: {"Sigma Computing": "https://www.sigmacomputing.com/"}},
     {text: "distributed systems researcher @ SkyLab", links: {"SkyLab": "https://sky.cs.berkeley.edu/project/rollbaccine/"}},
     {text: "backend developer @ BerkeleyTime", links: {"BerkeleyTime": "https://berkeleytime.com"}},
     {text: "course staff for cs162", links: {"cs162": "https://cs162.eecs.berkeley.edu"}},
